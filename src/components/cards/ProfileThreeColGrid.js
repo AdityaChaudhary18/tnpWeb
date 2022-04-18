c01fd4e80c1c44ba5b54b8c69a89f6679db7ad42
AdityaChaudhary18 <sanjords@gmail.com>
2022-04-18 21:08:32 +0530

    add: teams

--- src/components/cards/ProfileThreeColGrid.js
+++ src/components/cards/ProfileThreeColGrid.js
@@ -3,23 +3,29 @@ import tw from "twin.macro";
 import styled from "styled-components";
 import { css } from "styled-components/macro";
 import { Container, ContentWithPaddingXl } from "components/misc/Layouts.js";
-import { SectionHeading, Subheading as SubheadingBase } from "components/misc/Headings";
-import {SectionDescription} from "components/misc/Typography";
-import { ReactComponent as TwitterIcon} from "images/twitter-icon.svg";
-import { ReactComponent as LinkedinIcon} from "images/linkedin-icon.svg";
+import {
+  SectionHeading,
+  Subheading as SubheadingBase,
+} from "components/misc/Headings";
+import { SectionDescription } from "components/misc/Typography";
+import { ReactComponent as TwitterIcon } from "images/twitter-icon.svg";
+import { ReactComponent as LinkedinIcon } from "images/linkedin-icon.svg";
 import { ReactComponent as GithubIcon } from "images/github-icon.svg";
 
-const HeadingContainer = tw.div``
-const Heading = tw(SectionHeading)``
-const Subheading = tw(SubheadingBase)`text-center mb-3`
-const Description = tw(SectionDescription)`mx-auto text-center`
+const HeadingContainer = tw.div``;
+const Heading = tw(SectionHeading)``;
+const Subheading = tw(SubheadingBase)`text-center mb-3`;
+const Description = tw(SectionDescription)`mx-auto text-center`;
 
-const Cards = tw.div`flex flex-wrap flex-row justify-center sm:max-w-2xl lg:max-w-5xl mx-auto`
-const Card = tw.div`mt-24 w-full sm:w-1/2 lg:w-1/3 flex flex-col items-center`
+const Cards = tw.div`flex flex-wrap flex-row justify-center sm:max-w-2xl lg:max-w-5xl mx-auto`;
+const Card = tw.div`mt-24 w-full sm:w-1/2 lg:w-1/3 flex flex-col items-center`;
 const CardImage = styled.div`
-  ${props => css`background-image: url("${props.imageSrc}");`}
-  ${tw`w-64 h-64 bg-contain bg-center rounded`}
-`
+  ${(props) =>
+    css`
+      background-image: url("${props.imageSrc}");
+    `}
+  ${tw`w-64 h-64 bg-contain bg-center rounded-full `}
+`;
 const CardContent = styled.div`
   ${tw`flex flex-col items-center mt-6`}
   .position {
@@ -28,7 +34,7 @@ const CardContent = styled.div`
   .name {
     ${tw`mt-1 text-xl font-medium text-gray-900`}
   9
-`
+`;
 
 const CardLinks = styled.div`
   ${tw`mt-6 flex`}
@@ -38,17 +44,18 @@ const CardLinks = styled.div`
       ${tw`fill-current w-6 h-6`}
     }
   }
-`
+`;
 
 export default ({
-  heading = "Meet These Fine Folks.",
-  subheading = "Our Team",
-  description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  heading = "Our Team",
+  position = "Co-ordinator",
+  // description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
   cards = [
     {
-      imageSrc: "https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&fit=facearea&facepad=2.95&w=512&h=512&q=80",
-      position: "Founder",
-      name: "Adam Cuppy",
+      imageSrc:
+        "https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2.0,f_auto,g_center,h_250,q_auto:good,w_250/v1/gcs/platform-data-dsc/avatars/siddhant_tiwari_fsZpvkz.jpg",
+      position: `${position}`,
+      name: "Siddhant Tiwari",
       links: [
         {
           url: "https://twitter.com",
@@ -65,9 +72,10 @@ export default ({
       ],
     },
     {
-      imageSrc: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&fit=facearea&facepad=2.95&w=512&h=512&q=80",
-      position: "Sr. Designer",
-      name: "Charlotte Hale",
+      imageSrc:
+        "https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2.0,f_auto,g_center,h_250,q_auto:good,w_250/v1/gcs/platform-data-dsc/avatars/ansh_tyagi_co83Tdk.JPG",
+      position: `${position}`,
+      name: "Ansh Tyagi",
       links: [
         {
           url: "https://twitter.com",
@@ -84,9 +92,10 @@ export default ({
       ],
     },
     {
-      imageSrc: "https://images.unsplash.com/photo-1517070208541-6ddc4d3efbcb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&fit=facearea&facepad=2.95&w=512&h=512&q=80",
-      position: "Jr. Designer",
-      name: "Silvester Wize",
+      imageSrc:
+        "https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2.0,f_auto,g_center,h_250,q_auto:good,w_250/v1/gcs/platform-data-dsc/avatars/pulkit_goyal_EA3nCJz.png",
+      position: `${position}`,
+      name: "Pulkit Goyal",
       links: [
         {
           url: "https://twitter.com",
@@ -103,9 +112,10 @@ export default ({
       ],
     },
     {
-      imageSrc: "https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&fit=facearea&facepad=2.95&w=512&h=512&q=80",
-      position: "Lead Developer",
-      name: "Himali Turn",
+      imageSrc:
+        "https://media-exp1.licdn.com/dms/image/C5103AQHhm6rXuc0Tig/profile-displayphoto-shrink_800_800/0/1585407660559?e=1655942400&v=beta&t=tE2JQqacaA20KtK5exPVh4GxuEnfxEU3giVkaYCrvFc",
+      position: `${position}`,
+      name: "Tushar Upadhayay",
       links: [
         {
           url: "https://twitter.com",
@@ -122,9 +132,10 @@ export default ({
       ],
     },
     {
-      imageSrc: "https://images.unsplash.com/photo-1546820389-44d77e1f3b31?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&fit=facearea&facepad=3.45&w=512&h=512&q=80",
-      position: "Sr. Developer",
-      name: "Troye Sivan",
+      imageSrc:
+        "https://media-exp1.licdn.com/dms/image/C4E03AQHJp-PRwjUv9w/profile-displayphoto-shrink_800_800/0/1650295638502?e=1655942400&v=beta&t=gUU-H7ql-xkitS1p1CinaUtQnx0ouS5ORxCJjokpz80",
+      position: `${position} & Developer`,
+      name: "Aditya Chaudhary",
       links: [
         {
           url: "https://twitter.com",
@@ -141,9 +152,10 @@ export default ({
       ],
     },
     {
-      imageSrc: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&fit=facearea&facepad=3.45&w=512&h=512&q=80",
-      position: "Quality Assurance",
-      name: "Holo Wo",
+      imageSrc:
+        "https://media-exp1.licdn.com/dms/image/C4E03AQGY1ah1gLqqLQ/profile-displayphoto-shrink_800_800/0/1650295199940?e=1655942400&v=beta&t=TJuWf7HipsUgE4waWNXHXMrNvigfOukA3vL9AsSveAU",
+      position: `Volunteer`,
+      name: "Vansh Kela",
       links: [
         {
           url: "https://twitter.com",
@@ -159,15 +171,14 @@ export default ({
         },
       ],
     },
-  ]
+  ],
 }) => {
   return (
     <Container>
       <ContentWithPaddingXl>
         <HeadingContainer>
-          {subheading && <Subheading>{subheading}</Subheading>}
-          {heading && <Heading>{heading}</Heading> }
-          {description && <Description>{description}</Description> }
+          {heading && <Heading>{heading}</Heading>}
+          {/* {description && <Description>{description}</Description>} */}
         </HeadingContainer>
         <Cards>
           {cards.map((card, index) => (
