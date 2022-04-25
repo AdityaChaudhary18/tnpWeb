696cdd384808175b1eafd4aa1ee55c8b5c98cc00
AdityaChaudhary18 <sanjords@gmail.com>
2022-04-25 21:55:42 +0530

    add: rec page

--- src/components/features/TwoColWithSteps.js
+++ src/components/features/TwoColWithSteps.js
@@ -2,27 +2,37 @@ import React from "react";
 import tw from "twin.macro";
 import styled from "styled-components";
 import { css } from "styled-components/macro"; //eslint-disable-line
-import { SectionHeading, Subheading as SubheadingBase } from "components/misc/Headings.js";
+import "./rec.css";
+import {
+  SectionHeading,
+  Subheading as SubheadingBase,
+} from "components/misc/Headings.js";
 import TeamIllustrationSrc from "images/team-illustration-2.svg";
 import { ReactComponent as SvgDotPattern } from "images/dot-pattern.svg";
+import rec from "../../images/rec.png";
+import rec1 from "../../images/rec1.png";
+import rec2 from "../../images/rec2.png";
+import rec3 from "../../images/rec3.png";
 
 const Container = tw.div`relative`;
 const TwoColumn = tw.div`flex flex-col md:flex-row justify-between max-w-screen-xl mx-auto py-20 md:py-24 items-center`;
 const Column = tw.div`w-full max-w-md mx-auto md:max-w-none md:mx-0`;
 const ImageColumn = tw(Column)`md:w-6/12 flex-shrink-0 relative`;
-const TextColumn = styled(Column)(props => [
+const TextColumn = styled(Column)((props) => [
   tw`md:w-6/12 mt-16 md:mt-0`,
-  props.textOnLeft ? tw`md:mr-12 lg:mr-16 md:order-first` : tw`md:ml-12 lg:ml-16 md:order-last`
+  props.textOnLeft
+    ? tw`md:mr-12 lg:mr-16 md:order-first`
+    : tw`md:ml-12 lg:ml-16 md:order-last`,
 ]);
 
-const Image = styled.img(props => [
+const Image = styled.img((props) => [
   props.imageRounded && tw`rounded`,
   props.imageBorder && tw`border`,
-  props.imageShadow && tw`shadow`
+  props.imageShadow && tw`shadow`,
 ]);
 
 const DecoratorBlob = styled(SvgDotPattern)(() => [
-  tw`w-20 h-20 absolute right-0 bottom-0 transform translate-x-1/2 translate-y-1/2 fill-current text-primary-500 -z-10`
+  tw`w-20 h-20 absolute right-0 bottom-0 transform translate-x-1/2 translate-y-1/2 fill-current text-primary-500 -z-10`,
 ]);
 
 const TextContent = tw.div`lg:py-8 text-center md:text-left`;
@@ -60,45 +70,32 @@ export default ({
   const defaultSteps = [
     {
       heading: "Register",
-      description: "Create an account with us using Google or Facebook."
+      description: "Create an account with us using Google or Facebook.",
     },
     {
       heading: "Download",
-      description: "Browse and Download the template that you like from the marketplace."
+      description:
+        "Browse and Download the template that you like from the marketplace.",
     },
     {
       heading: "Run",
-      description: "Follow the instructions to setup and customize the template to your needs."
-    }
+      description:
+        "Follow the instructions to setup and customize the template to your needs.",
+    },
   ];
 
   if (!steps) steps = defaultSteps;
 
   return (
-    <Container>
-      <TwoColumn>
-        <ImageColumn>
-          <Image src={imageSrc} imageBorder={imageBorder} imageShadow={imageShadow} imageRounded={imageRounded} />
-          {imageDecoratorBlob && <DecoratorBlob css={decoratorBlobCss} />}
-        </ImageColumn>
-        <TextColumn textOnLeft={textOnLeft}>
-          <TextContent>
-            <Subheading>{subheading}</Subheading>
-            <Heading>{heading}</Heading>
-            <Steps>
-              {steps.map((step, index) => (
-                <Step key={index}>
-                  <StepNumber>{(index+1).toString().padStart(2,'0')}</StepNumber>
-                  <StepText>
-                    <StepHeading>{step.heading}</StepHeading>
-                    <StepDescription>{step.description}</StepDescription>
-                  </StepText>
-                </Step>
-              ))}
-            </Steps>
-          </TextContent>
-        </TextColumn>
-      </TwoColumn>
-    </Container>
+    <>
+      {/* <div className="oneRec">
+        <img src={rec} alt="img" />
+      </div> */}
+      <div className="threeRec">
+        <img src={rec1} alt="image1" className="company" />
+        <img src={rec2} alt="image2" className="company" />
+        <img src={rec3} alt="image3" className="company" />
+      </div>
+    </>
   );
 };
